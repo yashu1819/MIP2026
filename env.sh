901753ffbd2b28e4208b838b198e743562cc6a70
Yashu <mailtoyashu75@gmail.com>
2025-12-25 18:24:40 +0530

    changes in env.sh

--- env.sh
+++ env.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-# 1. Clean up existing CUDA/NVIDIA installations
-echo "--- Cleaning up existing installations ---"
-sudo apt-get purge -y "*cuda*" "*cublas*" "*cufft*" "*cufile*" "*curand*" \
-                      "*cusolver*" "*cusparse*" "*gds-tools*" "*npp*" "*nvjpeg*" "nsight*"
-sudo apt-get autoremove -y
-sudo rm -rf /usr/local/cuda*
+# 1) Remove old CUDA 11 toolkit (not drivers)
+sudo apt remove --purge -y "cuda-11*" "cuda-toolkit-11*"
+sudo apt autoremove -y
+sudo rm -rf /usr/local/cuda-11*
+sudo rm -rf /usr/local/cuda
 
-# 2. Install C++ 12 (GCC/G++ 12)
-echo "--- Installing GCC/G++ 12 ---"
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get install -y gcc-12 g++-12
+# 2) Add CUDA 12.2 repo
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb -O cuda-keyring.deb
+sudo dpkg -i cuda-keyring.deb
+sudo apt update
 
-# Set GCC 12 as the default choice
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100 --slave /usr/bin/g++ g++ /usr/bin/g++-12
-sudo update-alternatives --set gcc /usr/bin/gcc-12
+# 3) Install CUDA 12.2 toolkit
+sudo apt install -y cuda-12-2 cuda-toolkit-12-2
 
-# 3. Install CUDA 12.4
-echo "--- Installing CUDA 12.4 ---"
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get install -y cuda-toolkit-12-4
+# 4) Install gcc-12 and g++-12
+sudo apt install -y gcc-12 g++-12 build-essential
 
-# 4. Adding to Paths
-echo "--- Updating .bashrc paths ---"
-# Remove old CUDA paths if they exist to prevent duplicates
-sed -i '/\/usr\/local\/cuda/d' ~/.bashrc
+# 5) Clean old PATH lines in bashrc (basic delete)
+sed -i '/cuda/d' ~/.bashrc
+sed -i '/CUDA/d' ~/.bashrc
+sed -i '/CC=/d' ~/.bashrc
+sed -i '/CXX=/d' ~/.bashrc
 
-# Append new paths
-echo 'export PATH=/usr/local/cuda-12.4/bin${PATH:+:${PATH}}' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.4/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+# 6) Add CUDA 12.2 environment
+echo 'export PATH=/usr/local/cuda-12.2/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export CC=/usr/bin/gcc-12' >> ~/.bashrc
+echo 'export CXX=/usr/bin/g++-12' >> ~/.bashrc
 
-echo "--- DONE! Please run 'source ~/.bashrc' and 'nvcc --version' to verify ---"
+# 7) Apply new environment to this session
+source ~/.bashrc
+
+echo ""
+echo "Done. Close and reopen terminal or run: source ~/.bashrc"
+echo "Check with: nvcc --version  |  gcc --version"
