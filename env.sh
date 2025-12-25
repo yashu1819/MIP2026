f747f46e27cf0b8b288fccc74969eaa7e6900f2b
Yashu <mailtoyashu75@gmail.com>
2025-12-26 01:05:26 +0530

    minor changes

--- env.sh
+++ env.sh
@@ -8,12 +8,13 @@ sudo rm -rf /usr/local/cuda
 
 # 2) Add CUDA 12.2 repo
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb -O cuda-keyring.deb
-sudo dpkg -i cuda-keyring.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+
 sudo apt update
 
 # 3) Install CUDA 12.2 toolkit
-sudo apt install -y cuda-12-2 cuda-toolkit-12-2
-
+sudo apt install -y cuda-toolkit
+sudo apt install -y nvidia-gds
 # 4) Install gcc-12 and g++-12
 sudo apt install -y gcc-12 g++-12 build-essential
 
@@ -24,8 +25,8 @@ sed -i '/CC=/d' ~/.bashrc
 sed -i '/CXX=/d' ~/.bashrc
 
 # 6) Add CUDA 12.2 environment
-echo 'export PATH=/usr/local/cuda-12.2/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PATH=${PATH}:/usr/local/cuda-13.0/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-13.0/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 echo 'export CC=/usr/bin/gcc-12' >> ~/.bashrc
 echo 'export CXX=/usr/bin/g++-12' >> ~/.bashrc
 
