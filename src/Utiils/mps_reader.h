ed81973bb9e9dd8fac5f4e12236c71940a6a0bfe
Rehan <136876503+RE-NY@users.noreply.github.com>
2026-04-23 19:30:13 +0530

    Update utiils file

--- src/Utiils/mps_reader.h
+++ src/Utiils/mps_reader.h
@@ -1,7 +1,7 @@
 #ifndef MPS_READER_H
 #define MPS_READER_H
 
-#include "milp_problem.h"
+#include "../Definition/milp_problem.h"
 #include <string>
 
 namespace milp {
@@ -10,7 +10,7 @@ struct MpsReader {
     // Very small helper: for now we support a built-in "example" instance.
     // Real MPS parsing is not implemented here.
     // Returns true if loaded, false otherwise.
-    bool loadFromFile(const std::string& filename, MILPProblem& out_problem);
+    bool loadFromFile(const std::string& filename, MIPProblem& out_problem);
 };
 
 } // namespace milp
